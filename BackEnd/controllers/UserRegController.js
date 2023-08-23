33397f42e5a3941e44801d5b37a471dcb6e44228
Nisar Khan <khannisarnk0786@gmail.com>
2023-08-23 12:01:32 +0530

    setup the user registration

--- BackEnd/controllers/UserRegController.js
+++ BackEnd/controllers/UserRegController.js
@@ -1,3 +1,39 @@
-const UserRegController = () => {}
+import UserRegModel from "../models/UserRegModel.js";
+const UserRegister = async (req, res) => {
+   const {userName, userEmail, userMobile, userPassword, rPassword} = req.body;
+   if (userName && userEmail && userMobile && userPassword & rPassword) {
+      if (userPassword == rPassword) {
+         try {
+            const doc =  new UserRegModel({
+               userName,
+               userEmail,
+               userMobile,
+               userPassword,
+            });
+            await doc.save();
+            res.status(201).send({
+               status: "Success",
+               message: "Record Inserted...",
+            });
+         }catch(err){
+            console.log(err);
+            res.send({status: "Error", message: "Unable to Reg."});
+         }
+      }else {
+         res.send({
+            status: "Password Issue",
+            message: "Password and Confirm password does not matched...",
+         });
+      };
+   }else {
+      res.send({
+         status: "Error",
+         message: "Please fill all the fields...",
+      });
+   };
+};
+const UserLogin = (req, res) => {
+   res.status(200).send({message: "Hello...2"});
+};
 
-export default UserRegController
+export default {UserRegister, UserLogin};
--- BackEnd/routes/UserRegRoute.js
+++ BackEnd/routes/UserRegRoute.js
@@ -2,5 +2,9 @@ import express from "express";
 const router = express.Router();
 import UserRegController from "../controllers/UserRegController.js";
 
-// routes
+// public routes
+router.post("/register", UserRegController.UserRegister);
+router.post("/login", UserRegController.UserLogin);
+
+// private routes
 export default router;
