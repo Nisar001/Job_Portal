551bf271ea489b33c64e066a599ac903d60f491d
Nisar Khan <khannisarnk0786@gmail.com>
2023-09-10 20:38:41 +0530

    make the login component and signup component in front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import UserReg from "./components/UserReg";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import UserReg from "./components/UserReg";
+import Login from "./components/pages/Login";
+import SignUp from './components/pages/SignUp';
+import DashBoard from './components/pages/DashBoard';
 
 function App() {
   return (
-    <>
-    <Header/>
-    <UserReg/>
-    <Footer />
-    </>
+   <Routes>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/signUp' element={<SignUp/>}/>
+    <Route path='/' element={<DashBoard/>}/>
+    <Route path='*' element={<DashBoard/>}/>
+   </Routes>
   );
 }
 
